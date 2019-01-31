35b418d1c50d4bb555737fcffb5e78084278d984
Gurkiran Singh Randhawa <gukiece@gmail.com>
2019-01-31 12:23:15 +0530

    trying to resolve http calls

--- src/signup.js
+++ src/signup.js
@@ -69,7 +69,7 @@ class SignUp extends React.Component {
     event.preventDefault();
     if (this.state.username !== "" && this.state.password !== "") {
       axios
-        .post("http://localhost:3000/register", {
+        .post("/register", {
           firstname: this.state.firstname,
           lastname: this.state.lastname,
           email: this.state.email,
